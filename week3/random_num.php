be21fb2fe44041b34898183e2bbc9142d1a756ca
SKY WONG <skywongweijun693@gmail.com>
2022-10-18 22:56:23 +0800

    done random_num_color.php (not change anything in index.php)

--- week3/random_num.php
+++ week3/random_num.php
@@ -13,16 +13,6 @@
             color: blue;
             font-style: italic;
         }
-
-        .num3 {
-            color: red;
-            font-weight: bold;
-        }
-
-        .num4 {
-            font-style: italic;
-            font-weight: bold;
-        }
     </style>
 </head>
 
@@ -37,14 +27,6 @@
 
     echo "<span class=\"num2\">";
     echo $y . "<br>";
-    echo "</span>";
-
-    echo "<span class=\"num3\">";
-    echo ($x + $y . "<br>");
-    echo "</span>";
-
-    echo "<span class=\"num4\">";
-    echo ($x * $y . "<br>");
     echo "</span>";
     ?>
 </body>
