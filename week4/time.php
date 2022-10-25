ae11fb09aeff6b5fb806b2972f4460814a1fcb5d
SKY WONG <skywongweijun693@gmail.com>
2022-10-25 10:46:06 +0800

    create time.php

---
+++ week4/time.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+</head>
+
+<body>
+    <?php
+    date_default_timezone_set("Asia/Kuala_Lumpur");
+    echo date('d-m-Y H:i:s'); //Returns IS
+    ?>
+</body>
+
+</html>
