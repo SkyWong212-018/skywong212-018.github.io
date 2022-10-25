2451fcbe11e74adef613a581a0ac932ae65849ef
SKY WONG <skywongweijun693@gmail.com>
2022-10-25 11:16:15 +0800

    add "is lunch time"

--- week4/time.php
+++ week4/time.php
@@ -8,7 +8,19 @@
 <body>
     <?php
     date_default_timezone_set("Asia/Kuala_Lumpur");
-    echo date('d-m-Y H:i:s'); //Returns IS
+    $t = date("H");
+    echo $t . "<br>";
+
+    if ($t >= "06" and $t < "12") {
+        echo " <br>Good Morning";
+    } else if ($t >= "12" and $t < "18") {
+        echo " <br>Good Afternoon";
+        if ($t == 12) {
+            echo " <br>Is Lunch Time.";
+        }
+    } else if ($t >= "18") {
+        echo " <br>Good Night";
+    }
     ?>
 </body>
 
