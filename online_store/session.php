814a5085671d988051216eb345ba86a149f25d24
SKY WONG <skywongweijun693@gmail.com>
2022-11-29 10:16:48 +0800

    done

--- online_store/session.php
+++ online_store/session.php
@@ -3,6 +3,6 @@
 //check is user have pass or not
 session_start();
 if (!isset($_SESSION['username'])) {
-    header("Location: http://localhost/webdev/online_store/login.php");
+    header("Location: http://localhost/webdev/online_store/login.php?action=denied");
 }
 ?>
