f20a6a8e1181a82cfa955d7c9508698161713898
SKY WONG <skywongweijun693@gmail.com>
2022-12-19 15:35:57 +0800

    done 90%

---
+++ online_store/order_read_one.php
@@ -0,0 +1,90 @@
+<?php include 'session.php'; ?>
+<!DOCTYPE HTML>
+<html>
+
+<head>
+    <title>Order Details</title>
+    <!-- Latest compiled and minified Bootstrap CSS (Apply your Bootstrap here -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+</head>
+
+<body>
+
+    <!-- container -->
+    <div class="container">
+        <div class="page-header">
+            <h1>Read Order Details</h1>
+        </div>
+
+        <!-- PHP read one record will be here -->
+        <?php
+        // get passed parameter value, in this case, the record ID
+        // isset() is a PHP function used to verify if a value is there or not
+        $order_id = isset($_GET['order_id']) ? $_GET['order_id'] : die('ERROR: Record ID not found.');
+
+        //include database connection
+        include 'config/database.php';
+
+        // read current record's data
+        try {
+            // prepare select query
+            $query = "SELECT order_details_id, order_id, product_id, quantity FROM order_details WHERE order_id = ? LIMIT 0,1";
+            $stmt = $con->prepare($query);
+
+            // this is the first question mark
+            $stmt->bindParam(1, $order_id);
+
+            // execute our query
+            $stmt->execute();
+
+            // store retrieved row to a variable
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            // values to fill up our form
+            $order_details_id = $row['order_details_id'];
+            $order_id = $row['order_id'];
+            $product_id = $row['product_id'];
+            $quantity = $row['quantity'];
+        }
+
+        // show error
+        catch (PDOException $exception) {
+            die('ERROR: ' . $exception->getMessage());
+        }
+        ?>
+
+
+        <!-- HTML read one record table will be here -->
+        <!--we have our html table here where the record will be displayed-->
+        <table class='table table-hover table-responsive table-bordered'>
+            <tr>
+                <td>Order Detail ID</td>
+                <td><?php echo htmlspecialchars($order_details_id, ENT_QUOTES);  ?></td>
+            </tr>
+            <tr>
+                <td>Order ID</td>
+                <td><?php echo htmlspecialchars($order_id, ENT_QUOTES);  ?></td>
+            </tr>
+            <tr>
+                <td>Product ID</td>
+                <td><?php echo htmlspecialchars($product_id, ENT_QUOTES);  ?></td>
+            </tr>
+            <tr>
+                <td>Quantity</td>
+                <td><?php echo htmlspecialchars($quantity, ENT_QUOTES);  ?></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td>
+                    <a href='order_read.php' class='btn btn-danger'>Back to read order summary</a>
+                </td>
+            </tr>
+        </table>
+
+
+    </div> <!-- end .container -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
