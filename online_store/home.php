f8638245ca5d1931ff7432c4b8f8c1417ce5f4ed
SKY WONG <skywongweijun693@gmail.com>
2022-12-29 16:29:17 +0800

    done 90%

--- online_store/home.php
+++ online_store/home.php
@@ -40,23 +40,6 @@
     $stmt = $con->prepare($query);
     $stmt->execute();
     $total_order = $stmt->rowCount();
-
-    //Lastest
-    $query = "SELECT c.first_name, c.last_name, c.username, o.order_date, o.total_amount 
-    FROM order_summary o 
-    INNER JOIN customers c 
-    ON c.customer_id = o.customer_id 
-    ORDER BY order_id DESC";
-    $stmt = $con->prepare($query);
-    $stmt->execute();
-    $row = $stmt->fetch(PDO::FETCH_ASSOC);
-
-    // values to fill up our form
-    $username = $row['username'];
-    $first_name = $row['first_name'];
-    $last_name = $row['last_name'];
-    $order_date = $row['order_date'];
-    $total_amount = $row['total_amount'];
     ?>
 
     <img src="img/nightcity 1.png" alt="nightcity" style="width:100%;">
@@ -66,21 +49,45 @@
 
     <div class="card card border-dark rounded-0">
         <ul class="list-group list-group-flush">
+            <!-- Total customer -->
             <li class="list-group-item text-white bg-dark rounded-0">
                 <h1 class="display-6">Total Number of Customers: <b><?php echo "$total_customer" ?></b></h1>
                 <a href="http://localhost/webdev/online_store/customer_read.php" class="btn btn-primary col-1 mb-3">Customer List</a>
             </li>
+
+            <!-- Total product -->
             <li class="list-group-item text-white bg-dark rounded-0">
                 <h1 class="display-6">Total Number of Products: <b><?php echo "$total_product" ?></b></h1>
                 <a href="http://localhost/webdev/online_store/product_read.php" class="btn btn-primary col-1 mb-3">Product List</a>
             </li>
+
+            <!-- Total order -->
             <li class="list-group-item text-white bg-dark rounded-0">
                 <h1 class="display-6">Total Number of Orders: <b><?php echo "$total_order" ?></b></h1>
                 <a href="http://localhost/webdev/online_store/order_read.php" class="btn btn-primary col-1 mb-3">Order List</a>
             </li>
+
+            <!-- Latest Order ID & Summary -->
             <li class="list-group-item text-white bg-dark rounded-0">
                 <h1 class="display-6">Latest Order ID & Summary</br></h1>
                 <hr>
+                <?php
+                $query = "SELECT c.first_name, c.last_name, c.username, o.order_date, o.total_amount 
+                FROM order_summary o 
+                INNER JOIN customers c 
+                ON c.customer_id = o.customer_id 
+                ORDER BY order_id DESC";
+                $stmt = $con->prepare($query);
+                $stmt->execute();
+                $row = $stmt->fetch(PDO::FETCH_ASSOC);
+                // values to fill up our form
+                $username = $row['username'];
+                $first_name = $row['first_name'];
+                $last_name = $row['last_name'];
+                $order_date = $row['order_date'];
+                $total_amount = $row['total_amount'];
+                ?>
+
                 <h3><?php echo "Username: $username" ?></h3>
                 <h3><?php echo "Customer Name: $first_name $last_name" ?></h3>
                 <h3><?php echo "Total Amount: RM $total_amount" ?></h3>
@@ -91,31 +98,101 @@
     </div>
 
     <div class="card-group">
+
+        <!-- Top 5 selling Products -->
         <div class="card text-white bg-dark rounded-0 border-dark">
             <img class="card-img-top" src="img/nightcity 2.png" alt="Card image cap" height="630" width="auto">
             <div class="card-body">
                 <h5 class="card-title text-center">Top 5 selling Products</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <hr>
+                <p class="card-text text-center">
+                    <?php
+                    $query = "SELECT o.product_id, SUM(o.quantity) as totalquantity ,p.name as productname 
+                    FROM order_details o 
+                    INNER JOIN products p 
+                    ON o.product_id = p.id 
+                    GROUP BY o.product_id 
+                    ORDER BY totalquantity DESC LIMIT 5";
+                    $stmt = $con->prepare($query);
+                    $stmt->execute();
+                    // this is how to get number of rows returned
+                    $num = $stmt->rowCount();
+
+                    if ($num > 0) {
+                        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                            extract($row);
+                            echo $productname . "<br>";
+                            echo "<b>" . $totalquantity . "</b>" . "<br>";
+                        }
+                    }
+                    ?></p>
             </div>
             <div class="card-footer text-center">
                 <a href="http://localhost/webdev/online_store/product_read.php" class="btn btn-primary">Product List</a>
             </div>
         </div>
+
+        <!-- 3 Products that never purchased -->
         <div class="card text-white bg-dark rounded-0">
             <img class="card-img-top" src="img/nightcity 3.png" alt="Card image cap" height="630" width="auto">
             <div class="card-body">
                 <h5 class="card-title text-center">3 Products that never purchased</h5>
-                <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                <hr>
+                <h4 class="card-text text-center">
+                    <?php
+                    $query = "SELECT p.name FROM products p
+                    LEFT JOIN order_details o ON o.product_id = p.id
+                    WHERE o.product_id iS NULL LIMIT 3";
+                    $stmt = $con->prepare($query);
+                    $stmt->execute();
+                    $num = $stmt->rowCount();
+
+                    if ($num > 0) {
+                        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                            extract($row);
+                            echo $name . "</br>" . "</br>";
+                        }
+                    }
+                    ?>
+                </h4>
             </div>
             <div class="card-footer text-center">
                 <a href="http://localhost/webdev/online_store/product_read.php" class="btn btn-primary">Product List</a>
             </div>
         </div>
+
+        <!-- Order Summary with highest purchased amount -->
         <div class="card text-white bg-dark rounded-0">
             <img class="card-img-top" src="img/nightcity 4.png" alt="Card image cap" height="630" width="auto">
             <div class="card-body">
-                <h5 class="card-title text-center">The Order ID & Summary which has the highest purchased amount</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                <h5 class="card-title text-center">Order Summary with highest purchased amount</h5>
+                <hr>
+                <h4 class="card-text text-center">
+                    <?php
+                    $query = "SELECT c.first_name, c.last_name, c.username, SUM(o.total_amount) as total_amount
+                    FROM order_summary o 
+                    INNER JOIN customers c 
+                    ON c.customer_id = o.customer_id 
+                    ORDER BY total_amount DESC";
+                    $stmt = $con->prepare($query);
+                    $stmt->execute();
+                    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+                    // values to fill up our form
+                    $username = $row['username'];
+                    $first_name = $row['first_name'];
+                    $last_name = $row['last_name'];
+                    $total_amount = $row['total_amount'];
+
+                    echo "Username: " . "<b>" . $username . "</b>";
+                    echo "</br>";
+                    echo "</br>";
+                    echo "Customer Name: " . "<b>" . $first_name . ' ' . $last_name . "</b>";
+                    echo "</br>";
+                    echo "</br>";
+                    echo "Total Amount: <b>RM </b>" .  "<b>" . $total_amount . "</b>";
+                    ?>
+                </h4>
             </div>
             <div class="card-footer text-center">
                 <a href="http://localhost/webdev/online_store/order_read.php" class="btn btn-primary">Order List</a>
